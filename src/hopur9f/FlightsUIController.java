68ecd40f643ac6a61f7e268690a532e7cccfd205
astalaramagg <asta.magg94@gmail.com>
2018-03-25 14:21:54 +0000

    Bætti við CSS skrá

--- src/hopur9f/FlightsUIController.java
+++ src/hopur9f/FlightsUIController.java
@@ -7,26 +7,18 @@ package hopur9f;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 
 /**
+ * FXML Controller class
  *
- * @author sigrundish
+ * @author astalara
  */
-public class FXMLDocumentController implements Initializable {
-    
-    @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+public class FlightsUIController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
